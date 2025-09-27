e6058b2a6452d4e22cc9e3de9a8c8c06d29a15c7
Akwasi <122885868+apoa5@users.noreply.github.com>
2025-09-27 04:09:03 +0000

    fetching data from poke api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
+import Card from './components/card';
+import './styles/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(()=>{
+    const fetchPokemons = async ()=> {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
+        const data = await response.json();
+        
+        const pokemonDetails = await Promise.all(
+          data.results.map(async (pokemon) => {
+            const res = await fetch(pokemon.url);
+            return res.json();
+          })
+        )
+        setPokemons(pokemonDetails);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchPokemons();
+  }, []);
+
+  console.log(pokemons);
 
   return (
     <>
-      
+      {loading && <p>Loading...</p>}
+      {error && <p>Error:{error} </p>}
+      <div className="card-grid">
+        {pokemons.map((poke) => (
+          <Card
+            key={poke.id}
+            name={poke.name}
+            image={poke.sprites.other["official-artwork"].front_default}
+          />
+        ))}
+      </div>
     </>
   )
 }
